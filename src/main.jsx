479e7f0b7a66e57ff013c53951562296a83de17d
saikiran11461 <70562454+saikiran11461@users.noreply.github.com>
2022-05-02 16:59:18 +0530

    adding

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './Redux/store';
+// import { store } from './Redux/store';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<Provider store={store}>
+			<Provider >
 				<App />
 			</Provider>
 		</BrowserRouter>
